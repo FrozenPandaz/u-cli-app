b98fcd8bfe00c56120d054b2905d541302e85caa
FrozenPandaz <jasonjean1993@gmail.com>
2017-07-07 00:56:19 -0400

    Made changes

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const { AppModuleNgFactory } = require('./dist/main.bundle.js');
 const { renderModuleFactory, renderModule, platformServer, platformDynamicServer } = require('@angular/platform-server');
 
 console.log(require('./dist/main.bundle.js'));
-renderModuleFactory(AppModuleNgFactory, {
-  url: '/',
-  document: '<app-root></app-root>'
-})
-  .then(n => {
-    console.log(n);
-  });
+// renderModule(AppModule, {
+//   url: '/',
+//   document: '<app-root></app-root>'
+// })
+//   .then(n => {
+//     console.log(n);
+//   });
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,1 @@
-// import 'zone.js/dist/zone-node';
-export { AppModuleNgFactory } from './$$_gendir/app/app.module.ngfactory';
-
-// import { AppModule } from './app/app.module';
-// import { renderModule } from '@angular/platform-server';
-// console.time('hi');
-    // renderModule(AppModule, {
-    //   url: '/',
-    //   document: '<app-root></app-root>'
-    // })
-    //   .then(html => {
-    //     // console.timeEnd('hi');
-    //     console.log(html);
-    //   })
+export { AppModule } from './app/app.module';
